fee98a836ed11f15d37ee2477316367f4dd63828
Harry Kwon <harrykwon@harrykwon.dev>
2020-02-19 15:27:43 +0900

    FEATURE 8 - implemented lives system

---
+++ Assets/Scripts/Play/LivesTracker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivesTracker : MonoBehaviour
+{
+	public const int InitialLives = 3;
+	public int Lives { get; private set; }
+
+	public Transform IconsContainer;
+
+	public void ResetLives()
+	{
+		Lives = InitialLives;
+
+		for(int i=0; i<IconsContainer.childCount; i++)
+		{
+			IconsContainer.GetChild(i).gameObject.SetActive(true);
+		}
+	}
+
+	public void LoseLife()
+	{
+		Lives = Mathf.Max(0, Lives-1);
+		IconsContainer.GetChild(Lives).gameObject.SetActive(false);
+	}
+
+	public void GainLife()
+	{
+		Lives = Mathf.Min(InitialLives, Lives+1);
+		IconsContainer.GetChild(Lives - 1).gameObject.SetActive(true);
+	}
+
+	private void Start()
+	{
+		ResetLives();
+	}
+}
